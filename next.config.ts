f4f3b596a21b98d55adc8e3cc1d143487e8e3d41
Sushil sah <sahsushil757@gmail.com>
2026-05-31 12:10:54 +0545

    fix: Remove Sentry to fix 500 error

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
   images: {
@@ -13,10 +12,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withSentryConfig(nextConfig, {
-  silent: true,
-  org: "placeholder-org",
-  project: "placeholder-project",
-  widenClientFileUpload: true,
-  sourcemaps: { disable: true },
-});
+export default nextConfig;
